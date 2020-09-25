febd797f3650b7498652a20f025492597ffde181
mrys <1510218499@qq.com>
2020-09-25 23:10:43 +0800

    init

--- mrys-vertx-boot/mrys-vertx-boot-controller/src/main/java/top/mrys/vertx/boot/WebBoot.java
+++ mrys-vertx-boot/mrys-vertx-boot-controller/src/main/java/top/mrys/vertx/boot/WebBoot.java
@@ -1,11 +1,7 @@
 package top.mrys.vertx.boot;
 
-import io.vertx.core.Future;
-import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import top.mrys.vertx.common.config.ConfigRepo;
 import top.mrys.vertx.common.launcher.MyLauncher;
 import top.mrys.vertx.eventbus.EnableMicroClient;
 import top.mrys.vertx.http.starter.EnableHttp;
@@ -21,12 +17,6 @@ import top.mrys.vertx.http.starter.EnableHttp;
 public class WebBoot {
 
   public static void main(String[] args) {
-//    System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
-    Future<ApplicationContext> run = MyLauncher.run(WebBoot.class, args);
-/*    run.onSuccess(event -> {
-      String[] names = event.getBeanDefinitionNames();
-      Arrays.stream(names).forEach(log::info);
-      log.info("{}",ConfigRepo.getInstance().getData());
-    });*/
+     MyLauncher.run(WebBoot.class, args);
   }
 }
