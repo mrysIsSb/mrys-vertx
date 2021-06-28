fee88313853defbd4e993d6edc8c8f79fa887304
mrys <1510218499@qq.com>
2021-06-28 17:10:48 +0800

    add shell

--- mrys-vertx-boot/mrys-vertx-boot-demo/src/main/java/top/mrys/vertx/demo/Boot.java
+++ mrys-vertx-boot/mrys-vertx-boot-demo/src/main/java/top/mrys/vertx/demo/Boot.java
@@ -1,5 +1,12 @@
 package top.mrys.vertx.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+import sun.applet.Main;
+
 import io.vertx.core.Launcher;
 import io.vertx.core.Vertx;
 import io.vertx.ext.shell.ShellService;
@@ -10,6 +17,7 @@ import io.vertx.ext.shell.term.TelnetTermOptions;
  * @author mrys
  * 2021/6/28
  */
+@SpringBootApplication
 public class Boot extends Launcher {
 
   /**
@@ -36,6 +44,25 @@ public class Boot extends Launcher {
   }
 
   public static void main(String[] args) {
+    ConfigurableApplicationContext run = SpringApplication.run(Boot.class, args);
+    for (String name : run.getBeanDefinitionNames()) {
+      System.out.println(name);
+    }
+    System.out.println("-------------------------");
+    GenericApplicationContext context = new GenericApplicationContext();
+    context.setParent(run);
+    context.registerBean(MainVerticle.class);
+    context.refresh();
+    for (String name : context.getBeanDefinitionNames()) {
+      System.out.println(name);
+    }
+    System.out.println(context.getBean(Boot.class));
+    System.out.println(context.getBean(MainVerticle.class));
+    System.out.println("-------------------------");
+    context.close();
+    for (String name : context.getBeanDefinitionNames()) {
+      System.out.println(name);
+    }
     new Boot().dispatch(args);
   }
 }
