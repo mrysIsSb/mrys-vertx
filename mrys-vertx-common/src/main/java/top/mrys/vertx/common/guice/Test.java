4e58790352b18f89954364d6c52b0661625d665b
mrys <1510218499@qq.com>
2021-08-01 18:01:02 +0800

    :sparkles: add spring

---
+++ mrys-vertx-common/src/main/java/top/mrys/vertx/common/guice/Test.java
@@ -0,0 +1,36 @@
+package top.mrys.vertx.common.guice;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Stage;
+import top.mrys.vertx.common.ChannelContext;
+
+/**
+ * @author mrys
+ * @date 2021/7/30
+ */
+public class Test {
+
+  public static void main(String[] args) {
+    Injector injector = Guice.createInjector(Stage.PRODUCTION, new MyModule());
+    Injector injector1 = injector.createChildInjector(new MyModule2());
+    injector.getElements().forEach(System.out::println);
+    System.out.println("-----------------------------------------------------");
+    injector1.getElements().forEach(System.out::println);
+  }
+
+}
+
+class MyModule extends AbstractModule {
+
+
+  @Override
+  protected void configure() {
+    bind(ChannelContext.class);
+  }
+}
+class MyModule2 extends AbstractModule {
+
+
+}
